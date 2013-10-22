c47670bf1194324c44eb6b614874c89763098be7
Greg Look <greg@greg-look.net>
2013-10-22 00:58:08 -0700

    Implement macro for defining command trees.
    Help command is still broken in at least some cases.

--- src/mvxcvi/vault/tool.clj
+++ src/mvxcvi/vault/tool.clj
@@ -1,5 +1,6 @@
 (ns mvxcvi.vault.tool
-  (:require [clojure.tools.cli :refer [cli]])
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [cli]])
   (:gen-class :main true))
 
 ;; Vault commands need to know:
@@ -17,25 +18,14 @@
 ;; $ vault object ...
 ;; $ vault group ...
 
-;; So what's the algorithm?
-;; - scan arguments for first occurrance of subcommand key
-;;   NOTE: this means that an argument to an option CANNOT be any subcommand name!
-;;   e.g. $ vault --some-option blob --another-option foo group show ...
-;;        $ vault [--some-option] [blob --another-option foo ...]
-;;
-;; - run cli with the specs for this node on the pre-command args
-;; - if the subcommand is 'help', change it to :help true in the opts map and recur
-;; - if a subcommand was chosen, recurse with the subcommand node, the subcommand args, and the merged option map
-;; - if no subcommand was chosen and :help is set, print the help message for this node
-;; - if no subcommand was chosen and the node has an :action, execute the action with the opts and remaining args
-;; - otherwise, error.
 
 
+;; COMMAND TREE STRUCTURE
 
-
-; command node:
+; command nodes look like this:
 #_
-{:usage "vault [global opts] <command> [command args]"
+{:name "vault"
+ :usage "vault [global opts] <command> [command args]"
  :desc "Command-line tool for the vault data store."
  :specs [["--blob-store" "Path to blob-store configuration." ...]]
  :init (fn [opts] ...)
@@ -43,7 +33,52 @@
  :action (fn [opts args] ...)}
 
 
-(defn handle-command
+(def ^:private ^:dynamic *command-branch*
+  "Vector containing the command names on the current branch."
+  [])
+
+
+(defmacro ^:private command
+  "Simple macro for building readable command trees."
+  [usage desc & more]
+  (let [[cname usage] (string/split usage #" " 2)
+        [specs more] (split-with vector? more)
+        command {:name cname
+                 :usage `(string/join " " (concat *command-branch* [~usage]))
+                 :desc desc
+                 :specs (vec specs)}
+        elements (group-by first (filter list? more))]
+    (when (> (count (elements 'init)) 1)
+      (throw (IllegalArgumentException.
+               (str "Multiple `init` elements in command definition: " (elements 'init)))))
+    (when (> (count (elements 'action)) 1)
+      (throw (IllegalArgumentException.
+               (str "Multiple `action` elements in command definition: " (elements 'action)))))
+    (when (and (elements 'command) (elements 'action))
+      (throw (IllegalArgumentException.
+               (str "Command definition contains both `command` and `action` elements: " more))))
+    (let [assoc-fn (fn [cmd [k & body]]
+                     (if k
+                       (if (symbol? (first body))
+                         (assoc cmd (keyword k) (first body))
+                         (assoc cmd (keyword k) `(fn ~@body)))
+                       cmd))
+          command (reduce assoc-fn command
+                          [(first (elements 'init))
+                           (first (elements 'action))])]
+      (if-let [subcommands (elements 'command)]
+        `(binding [*command-branch* (conj *command-branch* ~(:name command))]
+           (->>
+             ~(vec subcommands)
+             (map (juxt :name identity))
+             (into {})
+             (assoc ~command :commands)))
+        `(binding [*command-branch* (conj *command-branch* ~(:name command))]
+           ~command)))))
+
+
+(defn- handle-command
+  "Handles a sequence of arguments following a command tree structure."
   [command opts args]
   (let [subcommand-names (into #{} (cons "help" (keys (:commands command))))
 
@@ -66,66 +101,48 @@
                @new-opts
                subcommand-args)) ; NOTE: ignores unknown args before the subcommand
       (cond (:help opts) (do (println banner) (System/exit 0))
-            (:action command) ((:action command) opts command-args)
+            (:action command) ((:action command) @new-opts command-args)
             :else (do (println banner) (System/exit 1))))))
-
-
-#_
-(command "vault [global opts] <command> [command args]"
-  "Command-line tool for the vault data store."
-
-  ["--blob-store" "Path to blob-store configuration."
-   :default "~/.config/vault/blob-store.edn"]
-  ["-v" "--verbose" "Show extra debugging messages."
-   :flag true :default false]
-  ["-h" "--help" "Show usage information."
-   :flag true :default false]
-
-  (init [opts]
-    (-> opts
-        (assoc :blob-store (slurp (:blob-store opts)))))
-
-  (command "blob [opts] <action> [args]"
-    "Blob storage command."
-
-    (command "list [opts]"
-      "Enumerate the stored blobs."
-      ; ... filtering/range options
-      (action blob-tool/list-blobs))
-
-    (command "info <blobref>"
-      "Show information about a stored blob."
-      (action blob-tool/blob-info))
-
-    (command "get <blobref>"
-      "Print the contents of a blob to stdout."
-      (action blob-tool/get-blob))
-
-    (command "put"
-      "Store a blob of data and print the resulting blobref."
-      (action blob-tool/put-blob))))
 
 
 
 ;; COMMAND LINE INTERFACE
 
+(def command-tree
+  (command "vault [global opts] <command> [command args]"
+    "Command-line tool for the vault data store."
+
+    ["--blob-store" "Path to blob-store configuration."
+     :default "~/.config/vault/blob-store.edn"]
+    ["-v" "--verbose" "Show extra debugging messages."
+     :flag true :default false]
+    ["-h" "--help" "Show usage information."
+     :flag true :default false]
+
+    #_ (init [opts]
+      (-> opts
+          (assoc :blob-store (slurp (:blob-store opts)))))
+
+    (command "blob [opts] <action> [args]"
+      "Blob storage command."
+
+      (command "list [opts]"
+        "Enumerate the stored blobs."
+        ; ... filtering/range options
+        (action vector #_ blob-tool/list-blobs))
+
+      (command "info <blobref>"
+        "Show information about a stored blob."
+        (action vector #_ blob-tool/blob-info))
+
+      (command "get <blobref>"
+        "Print the contents of a blob to stdout."
+        (action vector #_ blob-tool/get-blob))
+
+      (command "put"
+        "Store a blob of data read from stdin and print the resulting blobref."
+        (action vector #_ blob-tool/put-blob)))))
+
+
 (defn -main [& args]
-  (let [[opts args banner]
-        (cli
-          args
-          "Usage: vault [global opts] <system> <command> [command args]"
-          ["--blob-store" "Path to blob-store configuration."
-           :default "~/.config/vault/blob-store.edn"]
-          ["-v" "--verbose" "Show extra debugging messages."
-           :flag true :default false]
-          ["-h" "--help"    "Show usage information."
-           :flag true :default false])]
-    (when (:help opts)
-      (println banner)
-      (System/exit 0))
-    ;; TODO: command-line processing
-    (println "Opts: " opts)
-    (let [[system action & args] args]
-      (println "System: " system)
-      (println "Action: " action)
-      (println "Args: " args))))
+  (handle-command command-tree {} args))
