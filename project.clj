7acf8b0692937d90367f963945327b68c03fa84b
Greg Look <greg@greg-look.net>
2013-10-23 13:53:14 -0700

    Release project into the public domain.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject vault "0.1.0-SNAPSHOT"
   :description "Content-addressible datastore."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.4"]
                  [digest "1.4.3"]]
