14e40ca8bf83d3f72cc258d91167e2abafacdca0
Greg Look <greg@greg-look.net>
2013-10-30 16:01:33 -0700

    Implement printing for most EDN types.
    This includes support for a TaggedValue protocol which should be
    extended by types which have no representation in basic EDN types.

--- src/vault/print.clj
+++ src/vault/print.clj
@@ -22,7 +22,7 @@
 (defn- color-text
   "Constructs a text doc, which may be colored if *colored-output* is true."
   [text & codes]
-  (if *colored-output*
+  (if (and *colored-output* (not (empty? codes)))
     [:span [:pass (ansi/esc codes)] text [:pass (ansi/escape :none)]]
     text))
 
@@ -34,13 +34,47 @@
 
 
 
+;; TOTAL-ORDERING COMPARATOR
+
+(defn total-order
+  "Comparator function that provides a total-ordering of EDN values."
+  [a b]
+  (compare (pr-str a) (pr-str b))) ; FIXME: dirty hack
+
+
+
+;; EDN-TAGGED PROTOCOL
+
+(defprotocol TaggedValue
+  (edn-tag [this] "Return the tag symbol to apply to the value.")
+  (edn-value [this] "Return the value to pass to the tag."))
+
+
+(extend-protocol TaggedValue
+  java.util.Date
+  (edn-tag [this] 'inst)
+  (edn-value [this]
+    (let [utc-format (doto (java.text.SimpleDateFormat.
+                             "yyyy-MM-dd'T'HH:mm:ss.SSS-00:00")
+                       (.setTimeZone (java.util.TimeZone/getTimeZone "GMT")))]
+      (.format utc-format this))))
+
+
+
 ;; DISPATCH MULTIMETHOD
+
+(defn- canonize-dispatch
+  [value]
+  (if (extends? TaggedValue (class value))
+    :tagged-value
+    (type value)))
+
 
 (defmulti canonize
   "Converts the given value into a 'canonical' structured document, suitable
   for printing with fipp. This method also supports ANSI color escapes for
   syntax highlighting if desired."
-  type)
+  #'canonize-dispatch)
 
 
 (defmacro ^:private color-primitive
@@ -50,33 +84,64 @@
      (color-text (pr-str value#) ~@codes)))
 
 
-(color-primitive nil :red)
-(color-primitive java.lang.Boolean :bold :blue)
-(color-primitive java.lang.Number :cyan)
+(color-primitive nil :bold :black)
+(color-primitive java.lang.Boolean :cyan)
+(color-primitive java.lang.Number :green)
 (color-primitive java.lang.Character :bold :magenta)
 (color-primitive java.lang.String :bold :magenta)
 (color-primitive clojure.lang.Keyword :bold :yellow)
+(color-primitive clojure.lang.Symbol)
 
 
 (defmethod canonize clojure.lang.ISeq
   [s]
+  (let [elements (if (symbol? (first s))
+                   (cons (color-text (str (first s)) :bold :blue)
+                         (map canonize (rest s)))
+                   (map canonize s))]
+    [:group
+     (delimiter "(")
+     [:align (interpose :line elements)]
+     (delimiter ")")]))
+
+
+(defmethod canonize clojure.lang.IPersistentVector
+  [v]
   [:group
-   (delimiter "(")
-   [:align (interpose :line (map canonize s))]
-   (delimiter ")")])
+   (delimiter "[")
+   [:align (interpose :line (map canonize v))]
+   (delimiter "]")])
 
 
-; vector
+(defmethod canonize clojure.lang.IPersistentSet
+  [s]
+  (let [entries (sort total-order (seq s))]
+    [:group
+     (delimiter "#{")
+     [:align (interpose :line (map canonize entries))]
+     (delimiter "}")]))
 
-; Maps should print their entries in sorted key order.
 
-; Canonical sets should print their entries in sorted order.
+(defmethod canonize clojure.lang.IPersistentMap
+  [m]
+  (let [entries (sort-by first total-order (seq m))
+        entries (for [[k v] entries]
+                  [:span (canonize k) " " (canonize v)])]
+    [:group
+     (delimiter "{")
+     [:align (interpose [:span "," :line] entries)]
+     (delimiter "}")]))
 
+
+(defmethod canonize :tagged-value
+  [v]
+  [:span (color-text (str \# (edn-tag v)) :red) " " (canonize (edn-value v))])
 
 
 (defmethod canonize :default
   [value]
-  [:text (str \< (.getName (class value)) \space (pr-str value) \>)])
+  [:span "<" (color-text (.getName (class value)) :green)
+   (str \space (pr-str value) \>)])
 
 
 (defprinter cprint canonize {:width 80})
