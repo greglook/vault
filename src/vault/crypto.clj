0e7e05126c1fe7a30d180c8bcffe026233ba145a
Greg Look <greg@greg-look.net>
2013-11-04 12:59:07 -0800

    Checking in a bunch of crypto work.

--- src/vault/crypto.clj
+++ src/vault/crypto.clj
@@ -6,92 +6,145 @@
     (org.bouncycastle.jce.provider BouncyCastleProvider)
     (org.bouncycastle.openpgp PGPUtil
                               PGPPublicKey
-                              PGPPublicKeyRing
                               PGPPublicKeyRingCollection
-                              PGPSecretKeyRingCollection)))
+                              PGPSecretKey
+                              PGPSecretKeyRingCollection
+                              PGPSignature
+                              PGPSignatureGenerator
+                              )))
 
 
+; Install the BouncyCastle security provider.
 (Security/addProvider (new BouncyCastleProvider))
 
 
-(def ^:private public-key-algorithms
-  "Map of numeric codes to public-key algorithm names."
-  (->> (.getFields org.bouncycastle.bcpg.PublicKeyAlgorithmTags)
-       (map #(vector (.getInt % nil) (.getName %)))
+
+;; CONSTANTS
+
+(def ^:private security-provider "BC")
+
+
+(def ^:dynamic *hash-algorithm*
+  "Digest algorithm used for creating signatures."
+  "SHA1")
+
+
+(def hash-algorithms
+  "Map of numeric codes to available hash algorithm names."
+  (->> (.getFields org.bouncycastle.bcpg.HashAlgorithmTags)
+       (map #(vector (.getName %) (.getInt % nil)))
        (into {})))
 
 
-
-;; KEY FUNCTIONS
-
-(defn key-algorithm
-  "Returns the standard algorithm name for the key."
-  [k]
-  (.getAlgorithm k))
+(def public-key-algorithms
+  "Map of numeric codes to available public-key algorithm names."
+  (->> (.getFields org.bouncycastle.bcpg.PublicKeyAlgorithmTags)
+       (map #(vector (.getName %) (.getInt % nil)))
+       (into {})))
 
 
-(defn key-format
-  "Returns the name of the primary encoding format of the key."
-  [k]
-  (.getFormat k))
-
-
-(defn encode-key
-  "Returns the key in its primary encoding format."
-  [k]
-  (.getEncoded k))
-
-
-(defn private-key
-  "Converts the argument to a private key."
-  [k]
-  (cond (instance? PrivateKey k) k
-        (instance? KeyPair k) (.getPrivate k)
-        :else (throw (IllegalArgumentException.
-                       (str "Unknown private-key type: " k)))))
-
-
-(defn public-key
-  "Converts the argument to a public key."
-  [k]
-  (cond (instance? PublicKey k) k
-        (instance? KeyPair k) (.getPublic k)
-        :else (throw (IllegalArgumentException.
-                       (str "Unknown public-key type: " k)))))
+(defn- algorithm-name
+  "Look up the name of an algorithm given the numeric code."
+  [codes code]
+  (some (fn [[k i]] (if (= i code) k)) codes))
 
 
 
 ;; TEST FUNCTIONALITY
 
-(defn load-public-keys
-  "Loads the public keys present in the keyring file named by the path."
-  [path]
-  (with-open [input (PGPUtil/getDecoderStream
-                      (io/input-stream path))]
-    (->> input
-         (new PGPPublicKeyRingCollection)
-         .getKeyRings
-         iterator-seq
-         (mapcat #(iterator-seq (.getPublicKeys %))))))
+(defn key-id
+  "Coerce argument into a PGP key identifier."
+  [x]
+  (cond (integer? x) x
+        (string? x) (Long/parseLong x 16)
+        (instance? PGPPublicKey x) (.getKeyID x)
+        (instance? PGPSecretKey x) (.getKeyID x)
+        :else (throw (IllegalArgumentException.
+                       (str "Don't know how to make key id from: " x)))))
 
 
-(defn load-secret-keys
-  "Loads the private keyrings present in the keyring file named by the path."
-  [path]
+(defn- key-algorithm-code
+  "Returns numeric algorithm code from key argument."
+  [k]
+  (cond (instance? PGPPublicKey k) (.getAlgorithm k)
+        (instance? PGPSecretKey k) (.getAlgorithm (.getPublicKey k))
+        :else (throw (IllegalArgumentException.
+                       (str "Don't know how to get key algorithm from: " k)))))
+
+(defn key-algorithm
+  "Returns the name of the algorithm used by the given key."
+  [k]
+  (algorithm-name public-key-algorithms (key-algorithm-code k)))
+
+
+(defn signing-key? [k]
+  (.isSigningKey k))
+
+
+(defn load-secret-key
+  "Loads a secret key from the given keyring file."
+  [keyring-file id]
   (with-open [input (PGPUtil/getDecoderStream
-                      (io/input-stream path))]
-    (->> input
-         (new PGPSecretKeyRingCollection)
-         .getKeyRings
-         iterator-seq
-         (mapcat #(iterator-seq (.getSecretKeys %))))))
+                      (io/input-stream keyring-file))]
+    (-> (PGPSecretKeyRingCollection. input)
+        (.getSecretKey (key-id id)))))
+
+
+(defn- generate-signature
+  [data-source
+   hash-algorithm-code
+   key-algorithm-code
+   private-key]
+  (let [generator (PGPSignatureGenerator.
+                    key-algorithm-code
+                    hash-algorithm-code
+                    security-provider)]
+    (.init generator PGPSignature/BINARY_DOCUMENT private-key)
+    (with-open [data (io/input-stream data-source)]
+      (let [buffer (byte-array 256)]
+        (loop [n (.read data buffer)]
+          (when (> n 0)
+            (.update generator buffer 0 n)
+            (recur (.read data buffer))))))
+    (.generate generator)))
+
+
+(defn sign
+  [data-source secret-key passphrase]
+  (let [private-key (.extractPrivateKey
+                      secret-key
+                      passphrase
+                      security-provider)]
+    (generate-signature
+      data-source
+      (hash-algorithms *hash-algorithm*)
+      (key-algorithm-code secret-key)
+      private-key)))
 
 
 (defn print-key-info
   "Prints information about the given key."
   [k]
-  (println "Key id:" (Long/toHexString (.getKeyID k)))
-  (println "        Algorithm:" (public-key-algorithms (.getAlgorithm k) "UNKNOWN"))
-  (println "        Fingerprint:" (->> (.getFingerprint k)
-                                       (map (partial format "%02X"))
-                                       string/join)))
+  (println "Key id:" (Long/toHexString (key-id k))
+           (if (.isMasterKey k) "(master key)" "(subkey)"))
+  (when (instance? PGPSecretKey k)
+    (if (.isPrivateKeyEmpty k)
+      (println "    Private key is not available.")
+      (if (.isSigningKey k)
+        (println "    Private key is available for decryption and signing.")
+        (println "    Private key is available for decryption."))))
+  (let [public-key (if (instance? PGPSecretKey k) (.getPublicKey k) k)
+        strength (.getBitStrength public-key)
+        algorithm (or (key-algorithm public-key) "UNKNOWN")
+        fingerprint (->> (.getFingerprint public-key)
+                         (map (partial format "%02X"))
+                         string/join)]
+    (if (.isEncryptionKey public-key)
+      (println "    Public key is available for encryption.")
+      (println "    Public key is available."))
+    (println "    Algorithm:" algorithm (str \( strength " bits)"))
+    (println "    Fingerprint:"
+             (str (subs fingerprint 0 (- (count fingerprint) 8)) \/
+                  (subs fingerprint (- (count fingerprint) 8))))
+    (doseq [uid (.getUserIDs public-key)]
+      (println "    User ID:" uid))))
