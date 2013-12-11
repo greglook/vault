fcddaa1511e02fd628ba6f50d3193313163b107d
Greg Look <greg@greg-look.net>
2013-12-10 22:50:27 -0800

    Use snapshot version of puget.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/tools.cli "0.2.4"]
                  [org.bouncycastle/bcpg-jdk15on "1.49"]
                  [mvxcvi/directive "0.1.0"]
-                 [mvxcvi/puget "0.2.0"]
+                 [mvxcvi/puget "0.3.0-SNAPSHOT"]
                  [digest "1.4.3"]
                  [fipp "0.4.1"]]
   :main vault.tool)
