6401d743402b970b475a9bc55b75b43cfd16e70b
Greg Look <greg@greg-look.net>
2013-12-23 19:33:26 -0800

    Update the BlobStore and BlobCodec protocols.

--- src/vault/blob/core.clj
+++ src/vault/blob/core.clj
@@ -31,15 +31,22 @@
 (defprotocol BlobStore
   "Protocol for content storage providers, keyed by blobrefs."
 
-  (-list [this opts])
+  (-list [this opts]
+    "Enumerates the stored blobs with some filtering options.")
 
-  (-stat [this blobref])
+  (-stat [this blobref]
+    "Returns the stored status metadata for a blob.")
 
-  (-open [this blobref])
+  (-open [this blobref]
+    "Returns a vector of the blob status and an open input stream of the
+    stored data.")
 
-  (-store! [this blobref input-stream status])
+  (-store! [this blobref input-stream status]
+    "Stores the contents of the input stream for the blob, optionally with
+    associated status metadata.")
 
-  (-remove! [this blobref]))
+  (-remove! [this blobref]
+    "Returns true if the store contained the blob."))
 
 
 
--- src/vault/blob/filter.clj
+++ src/vault/blob/filter.clj
@@ -5,30 +5,34 @@
   "Protocol for filters which process blob content on the way to and from
   storage."
 
-  (codec-key
-    [this]
-    "Return the keyword which represents this codec.")
-
   (wrap-encoder
-    [this output-stream]
-    "Wrap the output stream with an encoding stream.")
+    [this status output-stream]
+    "Wrap the output stream with an encoding stream. Returns a vector of the
+    updated status map and wrapped output stream.")
 
   (wrap-decoder
-    [this input-stream]
-    "Wrap the input stream with a decoding stream."))
+    [this status input-stream]
+    "Wrap the input stream with a decoding stream. Returns a vector of the
+    updated status map and wrapped output stream."))
 
 
 (defn encode-stream
   "Returns a new output stream wrapped with the given encoders. Data written to
-  the stream will be processed by each codec in the order given."
-  [output-stream & encoders]
-  (reduce #(wrap-encoder %2 %1) output-stream (reverse encoders)))
+  the stream will be processed by each codec in the order given. Returns a
+  vector of the updated status map and the fully-encoded output stream."
+  [status output-stream codecs]
+  (let [wrap (fn [[status stream] codec]
+               (wrap-encoder codec status stream))]
+    (reduce wrap [status output-stream] (reverse codecs))))
 
 
 (defn decode-stream
   "Returns a new input stream wrapped with the given decoders. Data read from
   the stream will be processed by each codec in the reverse of the order given,
   so that the same sequence of codecs will work for both `encode-stream` and
-  `decode-stream`."
-  [input-stream & decoders]
-  (reduce #(wrap-decoder %2 %1) input-stream (reverse decoders)))
+  `decode-stream`. Returns a vector of the updated status map and the
+  fully-decoded input stream."
+  [status input-stream codecs]
+  (let [unwrap (fn [[status stream] codec]
+                 (wrap-decoder codec status stream))]
+    (reduce unwrap [status input-stream] (reverse codecs))))
