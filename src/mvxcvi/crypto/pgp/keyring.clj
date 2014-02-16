25c47dce9fd493c11608b0f6a54e509b070cfa98
Greg Look <greg@greg-look.net>
2014-02-15 23:19:38 -0800

    Split key provider functions into separate ns.

--- src/mvxcvi/crypto/pgp/keyring.clj
+++ src/mvxcvi/crypto/pgp/keyring.clj
@@ -1,9 +1,8 @@
 (ns mvxcvi.crypto.pgp.keyring
-  "Keyring provider protocols."
+  "Keyring store implementation."
   (:require
     byte-streams
     [clojure.java.io :as io]
-    [clojure.string :as string]
     [mvxcvi.crypto.pgp :as pgp :refer [KeyStore]])
   (:import
     (org.bouncycastle.openpgp
@@ -52,7 +51,7 @@
 
 
 
-;; KEYRING PROVIDER
+;; KEYRING STORE
 
 (defrecord PGPKeyring [pubring secring])
 
@@ -76,73 +75,3 @@
   "Constructs a PGPKeyring for the given keyring files."
   [pubring secring]
   (->PGPKeyring (io/file pubring) (io/file secring)))
-
-
-
-;; CACHING PROVIDER
-
-#_
-(defrecord PrivateKeyCache
-  [provider store]
-
-  KeyStore
-
-  (get-public-key
-    [this id]
-    (pgp/get-public-key (:provider this) id))
-
-  (get-secret-key
-    [this id]
-    (pgp/get-secret-key (:provider this) id))
-
-  (get-private-key
-    [this id]
-    (let [id (pgp/key-id id)]
-      (or (get @(:store this) id)
-          (when-let [privkey (pgp/get-private-key (:provider this) id)]
-            (swap! (:store this) assoc id privkey)
-            privkey))))
-
-  (get-private-key
-    [this id passphrase]
-    (let [id (pgp/key-id id)]
-      (or (get @(:store this) id)
-          (when-let [privkey (pgp/get-private-key (:provider this) id passphrase)]
-            (swap! (:store this) assoc id privkey)
-            privkey)))))
-
-
-#_
-(defn key-cache
-  "Wraps a key provider in a layer that keeps unlocked private keys in a map."
-  [provider]
-  (->PrivateKeyCache provider (atom {})))
-
-
-
-;; INTERACTIVE PROVIDER
-
-#_
-(defn interactive-unlocker
-  "Wraps a key provider in a layer that will request a passphrase on the
-  command-line when a private key needs to be unlocked."
-  [provider]
-  (reify KeyStore
-
-    (get-public-key
-      [_ id]
-      (pgp/get-public-key provider id))
-
-    (get-secret-key
-      [_ id]
-      (pgp/get-secret-key provider id))
-
-    (get-private-key
-      [_ id]
-      (let [id (pgp/key-id id)]
-        (println "Passphrase for private key " (pgp/key-id-hex id) ":")
-        (pgp/get-private-key provider id (read-line))))
-
-    (get-private-key
-      [_ id passphrase]
-      (pgp/get-private-key provider id passphrase))))
