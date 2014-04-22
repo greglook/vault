c1f87f5a869fa1e5b401485f37288e2defa92df1
Greg Look <greg@greg-look.net>
2014-04-22 00:59:15 -0700

    Simplify pgp blob-signer.

--- src/vault/data/signature.clj
+++ src/vault/data/signature.clj
@@ -3,39 +3,47 @@
   (:require
     [puget.data :refer [TaggedValue]]
     [vault.blob.core :as blob]
-    [vault.data.format.edn :as edn-data]
     [vault.data.format.pgp :as pgp-data]
     [mvxcvi.crypto.pgp :as pgp])
   (:import
     (org.bouncycastle.openpgp
-      PGPPrivateKey
       PGPPublicKey
-      PGPSecretKey
       PGPSignature)))
+
+
+(defn- load-pubkey
+  "Loads a PGP public key from a blob store."
+  [store pubkey-id]
+  (let [pubkey-blob (->> pubkey-id
+                         (blob/get store)
+                         pgp-data/read-blob)]
+    (when-not pubkey-blob
+      (throw (IllegalStateException.
+               (str "No public key blob stored for " pubkey-id))))
+    (when-not (= :pgp/public-key (:data/type pubkey-blob))
+      (throw (IllegalStateException.
+               (str "Blob " pubkey-id " is not a PGP public key"))))
+    (first (:data/values pubkey-blob))))
+
+
+(defn- sign-bytes
+  "Signs a byte array with a single public key."
+  [value-bytes store privkeys pubkey-id]
+  (let [pubkey (load-pubkey store pubkey-id)
+        privkey (privkeys (pgp/key-id pubkey))
+        pgp-sig (pgp/sign value-bytes privkey)]
+    {:key pubkey-id
+     :signature pgp-sig
+     :vault.data/type :vault/signature}))
 
 
 (defn blob-signer
   "Creates a function which signs a byte array with the PGP private key
   matching the public key identified by a hash-id. Returns a sequence
   containing the constructed signature map."
-  [store privkeys pubkey-id]
+  [store privkeys & pubkey-ids]
   (fn [value-bytes]
-    (let [pubkey-blob (->> pubkey-id
-                           (blob/get store)
-                           pgp-data/read-blob)]
-      (when-not pubkey-blob
-        (throw (IllegalStateException.
-                 (str "No public key blob stored for " pubkey-id))))
-      (when-not (= :pgp/public-key (:data/type pubkey-blob))
-        (prn pubkey-blob)
-        (throw (IllegalStateException.
-                 (str "Blob " pubkey-id " is not a PGP public key"))))
-      (let [pubkey (first (:data/values pubkey-blob))
-            privkey (privkeys (pgp/key-id pubkey))
-            pgp-sig (pgp/sign value-bytes privkey)]
-        [{:key pubkey-id
-          :signature pgp-sig
-          :vault.data/type :vault/signature}]))))
+    (map (partial sign-bytes value-bytes store privkeys) pubkey-ids)))
 
 
 (defn verify-blob
