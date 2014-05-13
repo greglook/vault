9ed455bf22fc8e72fdd6b0af36f1db260c27c861
Greg Look <greg@greg-look.net>
2014-05-12 22:37:32 -0700

    A few repl improvements.

--- repl/user.clj
+++ repl/user.clj
@@ -4,15 +4,24 @@
       [core :as time]
       [format :as ftime])
     [clojure.java.io :as io]
+    [clojure.repl :refer :all]
     [clojure.string :as str]
     [clojure.tools.namespace.repl :refer [refresh]]
-    [puget.printer :as puget]
+    (puget
+      [data]
+      [printer :as puget])
     [vault.blob.core :as blob]
     [vault.data.core :as data]
     [vault.entity.core :as entity]
     [vault.index.core :as index]
     (vault.tool
       [config :as tool-conf])))
+
+
+;; GENERAL CONFIG
+
+(puget.data/extend-tagged-map vault.blob.store.Blob 'vault.repl/blob)
+
 
 
 ;; VAULT SYSTEM
