7a8329d7cf780880450d8a89a1d81f47c36c2a48
Greg Look <greg@greg-look.net>
2014-06-04 22:40:16 -0700

    Add unit tests for entity state computation.

--- test/vault/entity/core_test.clj
+++ test/vault/entity/core_test.clj
@@ -123,3 +123,83 @@
     (let [datoms (mapcat entity/blob->datoms [root-a root-b update])]
       (puget/cprint (entity/entity-state (:id root-a) datoms))
       (puget/cprint (entity/entity-state (:id root-b) datoms)))))
+
+
+(deftest entity-state
+  (let [t (time/date-time 2014 5 14 3 20 36)
+        root-id (blob/hash (.getBytes "foo"))]
+    (is (thrown? RuntimeException
+          (entity/entity-state root-id
+            [(entity/->Datom :foo/bar root-id :attr "value" root-id t)]))
+        "Unknown datom operation throws an exception.")
+    (is (= {:vault.entity/id root-id}
+           (entity/entity-state root-id []))
+        "No datoms should produce an empty map.")
+    (is (= {:vault.entity/id root-id
+            :title "Entity Title"}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :title "Entity Title" root-id t)]))
+        "Setting a value for an unset attribute should produce an attribute with that value.")
+    (is (= {:vault.entity/id root-id
+            :title "Title Revision"}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :title "Entity Title" root-id t)
+              (entity/->Datom :attr/set root-id :title "Title Revision" root-id t)]))
+        "Setting an attribute to new value should produce an attribute with the new value.")
+    (is (= {:vault.entity/id root-id
+            :tags #{"foo"}}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/add root-id :tags "foo" root-id t)]))
+        "Adding a value to unset attribute should produce a set with a single entry.")
+    (is (= {:vault.entity/id root-id
+            :tags #{"bar" "foo"}}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/add root-id :tags "foo" root-id t)
+              (entity/->Datom :attr/add root-id :tags "bar" root-id t)]))
+        "Adding two values to an attribute should produce a set with two entries.")
+    (is (= {:vault.entity/id root-id
+            :flexible #{"123" "abc"}}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :flexible "123" root-id t)
+              (entity/->Datom :attr/add root-id :flexible "abc" root-id t)]))
+        "Adding a value to a single-valued attribute should produce a set with two entries.")
+    (is (= {:vault.entity/id root-id
+            :abc 123}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :abc 123 root-id t)
+              (entity/->Datom :attr/del root-id :foo nil root-id t)]))
+        "Deleting an unset attribute should produce the same map.")
+    (is (= {:vault.entity/id root-id}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :foo "123" root-id t)
+              (entity/->Datom :attr/del root-id :foo nil root-id t)]))
+        "Deleting an attribute with nil value should remove any value for attribute.")
+    (is (= {:vault.entity/id root-id}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :foo "123" root-id t)
+              (entity/->Datom :attr/del root-id :foo "123" root-id t)]))
+        "Deleting a value for an attribute with the same value should remove it.")
+    (is (= {:vault.entity/id root-id
+            :foo "123"}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/set root-id :foo "123" root-id t)
+              (entity/->Datom :attr/del root-id :foo "abc" root-id t)]))
+        "Deleting a value for an attribute with a different value should not remove it.")
+    (is (= {:vault.entity/id root-id}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/add root-id :foo "123" root-id t)
+              (entity/->Datom :attr/add root-id :foo "abc" root-id t)
+              (entity/->Datom :attr/del root-id :foo nil root-id t)]))
+        "Deleting a set attribute with nil value should remove the whole set.")
+    (is (= {:vault.entity/id root-id}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/add root-id :foo "123" root-id t)
+              (entity/->Datom :attr/del root-id :foo "123" root-id t)]))
+        "Deleting the only value from a set attribute should remove it.")
+    (is (= {:vault.entity/id root-id
+            :foo #{"abc"}}
+           (entity/entity-state root-id
+             [(entity/->Datom :attr/add root-id :foo "123" root-id t)
+              (entity/->Datom :attr/add root-id :foo "abc" root-id t)
+              (entity/->Datom :attr/del root-id :foo "123" root-id t)]))
+        "Deleting a value from a set attribute should remove the value.")))
