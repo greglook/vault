81c47f1a25c6ce64475182b45b0b2e0a5b680b51
Greg Look <greg@greg-look.net>
2014-06-27 21:10:33 -0700

    Minor require changes in tool system.

--- tool/vault/system.clj
+++ tool/vault/system.clj
@@ -2,22 +2,19 @@
   (:require
     (clj-time
       [core :as time]
-      [format :as ftime])
+      [format :as timef])
     [clojure.java.io :as io]
     [clojure.repl :refer :all]
     [clojure.string :as str]
     [clojure.tools.namespace.repl :refer [refresh]]
     [environ.core :refer [env]]
-    (puget
-      [data]
-      [printer :as puget])
+    [puget.printer :as puget]
     [vault.blob.core :as blob]
-    vault.blob.store
     [vault.data.core :as data]
     [vault.entity.core :as entity]
     [vault.index.core :as index]
     (vault.tool
-      [config :as config])))
+      [config :as conf])))
 
 
 ;; GENERAL CONFIG
@@ -32,10 +29,10 @@
 ;; VAULT SYSTEM
 
 (def config
-  (config/load-configs (env :vault-config "dev/config")))
+  (conf/load-configs (env :vault-config "dev/config")))
 
 
 (def blobs
-  (config/select-blob-store
+  (conf/select-blob-store
     (:blob-stores config)
     :default))
