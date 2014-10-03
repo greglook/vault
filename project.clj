528266b86c402302de2d21afa44cf654b13f904b
Greg Look <greg@greg-look.net>
2014-10-02 23:56:09 -0700

    Move some project config to the dev profile.

--- project.clj
+++ project.clj
@@ -4,41 +4,16 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
 
-  :plugins
-  [[codox "0.8.10"]
-   [lein-marginalia "0.8.0"]]
-
   :dependencies
   [[byte-streams "0.1.13"]
    [clj-time "0.8.0"]
+   [com.stuartsierra/component "0.2.2"]
    [environ "1.0.0"]
    [mvxcvi/clj-pgp "0.5.4"]
    [mvxcvi/puget "0.6.4"]
    [org.clojure/clojure "1.6.0"]
    [org.clojure/data.codec "0.1.0"]
    [prismatic/schema "0.3.0"]]
-
-  :aliases
-  {"docs" ["do" ["doc"]
-                ["marg"
-                 "--dir" "target/doc"
-                 "--file" "marginalia.html"
-                 "src/vault/blob/content.clj"
-                 "src/vault/blob/store.clj"
-                 "src/vault/blob/store/memory.clj"
-                 "src/vault/blob/store/file.clj"
-                 "src/vault/data/struct.clj"
-                 "src/vault/data/key.clj"
-                 "src/vault/data/edn.clj"
-                 "src/vault/data/signature.clj"
-                 "src/vault/entity/datom.clj"
-                 "src/vault/entity/schema.clj"
-                 "src/vault/entity/tx.clj"
-                 "src/vault/search/index.clj"
-                 "src/vault/search/index/brute.clj"
-                 "src/vault/search/index/memory.clj"
-                 "src/vault/search/catalog.clj"
-                 "src/vault/search/query.clj"]]}
 
   :codox
   {:defaults {:doc/format :markdown}
@@ -56,6 +31,33 @@
   {:coverage
    {:plugins
     [[lein-cloverage "1.0.2"]]}
+
+   :dev
+   {:plugins
+    [[codox "0.8.10"]
+     [lein-marginalia "0.8.0"]]
+
+    :aliases
+    {"docs" ["do" ["doc"]
+                  ["marg"
+                   "--dir" "target/doc"
+                   "--file" "marginalia.html"
+                   "src/vault/blob/content.clj"
+                   "src/vault/blob/store.clj"
+                   "src/vault/blob/store/memory.clj"
+                   "src/vault/blob/store/file.clj"
+                   "src/vault/data/struct.clj"
+                   "src/vault/data/key.clj"
+                   "src/vault/data/edn.clj"
+                   "src/vault/data/signature.clj"
+                   "src/vault/entity/datom.clj"
+                   "src/vault/entity/schema.clj"
+                   "src/vault/entity/tx.clj"
+                   "src/vault/search/index.clj"
+                   "src/vault/search/index/brute.clj"
+                   "src/vault/search/index/memory.clj"
+                   "src/vault/search/catalog.clj"
+                   "src/vault/search/query.clj"]]}}
 
    :tool
    {:dependencies
