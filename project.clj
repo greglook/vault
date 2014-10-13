a20ba61a075729dc9e2ad5b3fddd80548a49af17
Greg Look <greg@greg-look.net>
2014-10-12 18:18:32 -0700

    Simplify doc configuration in project file.

--- project.clj
+++ project.clj
@@ -16,12 +16,6 @@
    [org.clojure/data.codec "0.1.0"]
    [prismatic/schema "0.3.0"]]
 
-  :codox
-  {:defaults {:doc/format :markdown}
-   :output-dir "target/doc/codox"
-   :src-dir-uri "https://github.com/greglook/vault/blob/develop/"
-   :src-linenum-anchor-prefix "L"}
-
   :hiera
   {:path "target/doc/ns-hiera.png"
    :vertical? false
@@ -35,32 +29,22 @@
 
    :dev
    {:source-paths ["dev/src"]
+
     :plugins
     [[codox "0.8.10"]
      [lein-marginalia "0.8.0"]]
     :dependencies
     [[org.clojure/tools.namespace "0.2.7"]]
+
+    :codox
+    {:defaults {:doc/format :markdown}
+     :exclude #{user}
+     :output-dir "target/doc/codox"
+     :src-dir-uri "https://github.com/greglook/vault/blob/develop/"
+     :src-linenum-anchor-prefix "L"}
+
     :aliases
-    {"docs" ["do" ["doc"]
-                  ["marg"
-                   "--dir" "target/doc"
-                   "--file" "marginalia.html"
-                   "src/vault/blob/content.clj"
-                   "src/vault/blob/store.clj"
-                   "src/vault/blob/store/memory.clj"
-                   "src/vault/blob/store/file.clj"
-                   "src/vault/data/struct.clj"
-                   "src/vault/data/key.clj"
-                   "src/vault/data/edn.clj"
-                   "src/vault/data/signature.clj"
-                   "src/vault/entity/datom.clj"
-                   "src/vault/entity/schema.clj"
-                   "src/vault/entity/tx.clj"
-                   "src/vault/search/index.clj"
-                   "src/vault/search/index/brute.clj"
-                   "src/vault/search/index/memory.clj"
-                   "src/vault/search/catalog.clj"
-                   "src/vault/search/query.clj"]]}}
+    {"docs" ["do" "hiera" "doc" ["marg" "--multi" "--dir" "target/doc/marginalia"]]}}
 
    :tool
    {:source-paths ["tool"]
